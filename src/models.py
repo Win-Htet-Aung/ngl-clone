f07af490e801309b253981805325ba3f39288beb
Win Htet Aung <whaung1998@gmail.com>
2024-12-29 09:38:45 +0700

    create categories links messages

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from src.database import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
 
@@ -8,3 +8,32 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+
+class Link(Base):
+    __tablename__ = "links"
+
+    id = Column(Integer, primary_key=True, index=True)
+    url = Column(String, unique=True, index=True)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship("Category", back_populates="links")
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="links")
+    created_at = Column(DateTime, nullable=False)
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String)
+    link_id = Column(Integer, ForeignKey("links.id"))
+    link = relationship("Link", back_populates="messages")
+    created_at = Column(DateTime, nullable=False)
